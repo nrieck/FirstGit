221847581eaafc168b40e6aa519d06eba440a395
nrieck <nrieck@10.18.12.4>
2017-08-29 15:41:08 -0500

    Added second Statement to main

--- src/firstgit/FirstGit.java
+++ src/firstgit/FirstGit.java
@@ -12,6 +12,8 @@ public class FirstGit {
      */
     public static void main(String[] args) {
         System.out.println("Hello World");
+        System.out.println("Second comment");
     }
+    
     
 }
