db9d4e6aa41190790ec4bd5f044b5b75cc5f554b
nrieck <nrieck@10.18.12.4>
2017-08-31 13:50:45 -0500

    Very very last

--- src/firstgit/FirstGit.java
+++ src/firstgit/FirstGit.java
@@ -1,4 +1,8 @@
-
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
 package firstgit;
 
 /**
@@ -12,8 +16,12 @@ public class FirstGit {
      */
     public static void main(String[] args) {
 
+
         System.out.println("Hello World");
         System.out.println("Second comment");
+
+
+        // TODO code application logic here
 
     }
     
